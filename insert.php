69a7f792975079f0462c4e077cae167acbf9f472
Tech house <102593727+shalam678@users.noreply.github.com>
2023-06-22 08:48:19 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        h1 {color: blue;}
-    </style>
-    <?php
+<style>
+h1   {color: blue;}
+</style>
+ <?php
     // Prevent caching of the page
     header('Cache-Control: no-cache, no-store, must-revalidate');
     header('Pragma: no-cache');
@@ -14,39 +14,49 @@
     ?>
 </head>
 <body>
-    <center>
-        <?php
-        $conn = mysqli_connect("localhost", "root", "", "busticket");
-
-        if($conn === false){
-            die("ERROR: Could not connect. " . mysqli_connect_error());
-        }
-
-        if(isset($_POST['submit'])) {
-            $name = $_POST['fname'];
-            $father_name = $_POST['dname'];
-            $mother_name = $_POST['mname'];
-            $date_of_birth = $_POST['dob'];
-            $gender = $_POST['gender'];
-            $marital_status = $_POST['mstatus'];
-            $nationality = $_POST['nationality'];
-            $aadhar_number = $_POST['anumber'];
-            $phone_number = $_POST['pnumber'];
-            $email = $_POST['eaddress'];
-            $address = $_POST['address'];
-
-            $sql = "INSERT INTO wcm_members (name, father_name, mother_name, date_of_birth, gender, marital_status, nationality, aadhar_number, phone_number, email, address) VALUES ('$name', '$father_name', '$mother_name', '$date_of_birth', '$gender', '$marital_status', '$nationality', '$aadhar_number', '$phone_number', '$email', '$address')";
-
-            if(mysqli_query($conn, $sql)){
-                echo "<h1>Data Submitted Successfully</h1>";
-            } else{
-                echo "ERROR: Hush! Sorry $sql. " . mysqli_error($conn);
-            }
-        }
-        
-        // Close connection
-        mysqli_close($conn);
-        ?>
-    </center>
+	<center>
+		<?php
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => staff
+		$conn = mysqli_connect("localhost", "root", "", "busticket");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$name = $_REQUEST['fname'];
+		$father_name = $_REQUEST['dname'];
+		$mother_name = $_REQUEST['mname'];
+		$date_of_birth = $_REQUEST['dob'];
+		$gender = $_REQUEST['gender'];
+		$marital_status = $_REQUEST['mstatus'];
+		$nationality = $_REQUEST['nationality'];
+		$aadhar_number = $_REQUEST['anumber'];
+		$phone_number = $_REQUEST['pnumber'];
+		$email = $_REQUEST['eaddress'];
+		$address = $_REQUEST['address'];
+	
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO wcm_members(name,father_name,mother_name,date_of_birth,gender,marital_status,nationality,aadhar_number,phone_number,email,address) VALUES ('$name','$father_name','$mother_name','$date_of_birth','$gender','$marital_status','$nationality','$aadhar_number','$phone_number','$email','$address')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h1> Data Submitted Successfully</h1>";
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	</center>
 </body>
+
 </html>
+
